2a40c54094052873b795462eca3be447c374f8ff
the-coder <thuyeintun.zack@gmail.com>
2014-12-21 15:11:37 +0800

    Added webbrowser function
    When mouse is clicked on label, it will open browser and go to
    www.mmsecurity.net/forum

--- iptools.py
+++ iptools.py
@@ -1,4 +1,4 @@
-import sys, os, urllib, socket
+import sys, os, urllib, socket, webbrowser
 
 from PyQt4 import QtCore, QtGui, uic
 
@@ -16,7 +16,7 @@ class IP(QtGui.QMainWindow,form_class):
         self.statusbar.showMessage("Product of MSF (www.mmsecurity.net)")
 
         self.lblpic.setPixmap(QtGui.QPixmap(os.getcwd()+"/resources/msf.png"))
-
+        self.lblpic.mousePressEvent = self.openBrowser()
         self.show()
 
     def getlanip(self):
@@ -30,6 +30,10 @@ class IP(QtGui.QMainWindow,form_class):
             QtGui.QMessageBox.warning(self,"Error!","No Internet Access",buttons=QtGui.QMessageBox.Ok)
         else:
             self.txtwanip.setText(wanip)
+
+    def openBrowser(self):
+        browser = webbrowser.WindowsDefault()
+        browser.open_new("http://www.mmsecurity.net/forum")
 
 def main():
     app = QtGui.QApplication(sys.argv)
